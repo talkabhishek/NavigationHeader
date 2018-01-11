64e5f4be2ea6f35cf9f10aad27b2e469a4674e7a
Abhishek <abhishekpratap.singh@newgenapps.com>
2018-01-11 11:20:30 +0530

    Added Example use

---
+++ HeaderExample/HeaderExample/ViewController.swift
@@ -0,0 +1,26 @@
+//  
+//  ViewController.swift
+//  HeaderExample
+//  Using Swift 4.0
+//
+//  Created by Abhishek Singh on 11/01/18.
+//  Copyright © 2018 NGA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
